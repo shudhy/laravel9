b86f8eda64853836e51e9300a6cc6b7b922151d4
shudhy <shudhy@gmail.com>
2023-03-14 13:34:32 +0800

    menambakna migration table items

--- database/migrations/2023_02_25_122821_create_items_table.php
+++ database/migrations/2023_02_25_122821_create_items_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->integer('barcode');
+            $table->integer('stok_produk');
+            $table->integer('terjual_produk');
+            $table->string('nm_produk', 100);
+            $table->enum('aktif_produk', ['Y', 'N']);
+            $table->text('isi_produk');
+            $table->string('foto_produk', 100);
+            $table->double('harga_produk', 8, 2);
+            $table->integer('diskon_produk');
+            $table->decimal('berat_produk', $precision = 8, $scale = 2);
             $table->timestamps();
         });
     }
